a06987ff1f5e0d80a07ce86a2dda9dc12f01801f
sudarshan pawar <sudarshanpawar0007@gmail.com>
2026-03-28 18:49:49 +0530

    This is Register & Login Page script using Automation..

---
+++ src/Assignment/LoginModule.java
@@ -0,0 +1,42 @@
+package Assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class LoginModule {
+	public static void main(String[] args) throws InterruptedException {
+
+		// open the browser
+		ChromeDriver driver = new ChromeDriver();
+
+		// maximize the browser
+		driver.manage().window().maximize();
+
+		// enter into a webpage
+		driver.get("https://demowebshop.tricentis.com/");
+
+		// click on the login
+		driver.findElement(By.className("ico-login")).click();
+
+		// enter into the email text web element
+		driver.findElement(By.className("email")).sendKeys("sudarshanpawar0007@gmail.com");
+		Thread.sleep(2000);
+
+		// enter into the password text webElement
+		driver.findElement(By.name("Password")).sendKeys("Sudarshan@123");
+		Thread.sleep(2000);
+
+		// click on the remember me checkbox web element
+		driver.findElement(By.id("RememberMe")).click();
+		Thread.sleep(2000);
+
+		// click on the login button
+		driver.findElement(By.className("login-button")).click();
+
+		Thread.sleep(2000);
+
+		driver.close();
+
+	}
+
+}
---
+++ src/Assignment/RegisterModule.java
@@ -0,0 +1,44 @@
+package Assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RegisterModule {
+	public static void main(String[] args) throws InterruptedException {
+
+		// open the browser
+		ChromeDriver driver = new ChromeDriver();
+		// maximize the browser
+		driver.manage().window().maximize();
+		// Enter into webpage
+		driver.get("https://demowebshop.tricentis.com/");
+		Thread.sleep(2000);
+		// Enter click on register
+		driver.findElement(By.className("ico-register")).click();
+		Thread.sleep(2000);
+		// select the gender radio webelement
+		driver.findElement(By.id("gender-male")).click();
+		Thread.sleep(2000);
+		// enter into firstName textfield webelement
+		driver.findElement(By.id("FirstName")).sendKeys("Sudarshan");
+		Thread.sleep(2000);
+		// enter into LastName textfield webelement
+		driver.findElement(By.id("LastName")).sendKeys("Pawar");
+		Thread.sleep(2000);
+		// enter into Email textfield webelement
+		driver.findElement(By.id("Email")).sendKeys("sudarshanpawar0007@gmail.com");
+		Thread.sleep(2000);
+		// enter into Password textfield webelement
+		driver.findElement(By.id("Password")).sendKeys("Sudarshan@123");
+		Thread.sleep(2000);
+		// enter into ConfirmPassword textfield webelement
+		driver.findElement(By.id("ConfirmPassword")).sendKeys("Sudarshan@123");
+		Thread.sleep(2000);
+		// click on the Register Button webelement
+		driver.findElement(By.name("register-button")).click();
+		Thread.sleep(2000);
+		driver.close();
+
+	}
+
+}
