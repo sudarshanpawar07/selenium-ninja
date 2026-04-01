631934a70fa390454c43ae084ee8c21763e56b92
sudarshan pawar <sudarshanpawar0007@gmail.com>
2026-04-01 20:01:39 +0530

    i done some changes in the Script..

--- src/Locators/AbsoluteXpath.java
+++ src/Locators/AbsoluteXpath.java
@@ -6,15 +6,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AbsoluteXpath {
 	public static void main(String[] args) throws InterruptedException {
 		// open the browser
-				ChromeDriver driver = new ChromeDriver();
-				// maximize the browser
-				driver.manage().window().maximize();
-				// enter into web page
-				driver.navigate().to("https://demowebshop.tricentis.com/");
-				Thread.sleep(2000);
-				driver.findElement(By.xpath("/html/body/div[4]/div/div/div[2]/div[1]/ul/li[1]/a")).click();
-				Thread.sleep(2000);
-				driver.findElement(By.xpath("")).click();
+		ChromeDriver driver = new ChromeDriver();
+		// maximize the browser
+		driver.manage().window().maximize();
+		// enter into web page
+		driver.navigate().to("https://demowebshop.tricentis.com/");
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("/html/body/div[4]/div/div/div[2]/div[1]/ul/li[1]/a")).click();
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[3]/a/span[1]")).click();
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[4]/a/span[1]")).click();
+		Thread.sleep(2000);
+
+		driver.close();
 	}
 
 }
