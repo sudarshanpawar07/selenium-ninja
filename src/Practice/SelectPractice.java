12b53142c67c04eff2855ad032f513ebea5f0190
sudarshan pawar <sudarshanpawar0007@gmail.com>
2026-04-19 14:16:00 +0530

    This is todays Practice..

--- src/Practice/SelectPractice.java
+++ src/Practice/SelectPractice.java
@@ -1,5 +1,17 @@
 package Practice;
 
-public class SelectPractice {
+import org.openqa.selenium.WebDriver;
+
+
+import UtilityClass.Base;
+
+public class SelectPractice extends Base {
+	public static void main(String[] args) throws InterruptedException {
+
+		preCondition();
+		Thread.sleep(2000);
+		postCondition();
+
+	}
 
 }
