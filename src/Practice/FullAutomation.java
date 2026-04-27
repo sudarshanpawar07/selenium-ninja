0e8a23dea0d8251aab465366298e655c47445b38
sudarshan pawar <sudarshanpawar0007@gmail.com>
2026-04-27 15:35:17 +0530

    I done some modification in the previous script...

--- src/Practice/FullAutomation.java
+++ src/Practice/FullAutomation.java
@@ -3,15 +3,21 @@ package Practice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FullAutomation {
+import UtilityClass.Base;
+
+public class FullAutomation extends Base {
 	public static void main(String[] args) throws InterruptedException {
 
-		ChromeDriver driver = new ChromeDriver();
-		
-		driver.manage().window().maximize();
-		driver.get("https://demowebshop.tricentis.com/");
+		preCondition();
 		Thread.sleep(2000);
+		login();
 		driver.findElement(By.linkText("Books")).click();
+		Thread.sleep(2000);
+		driver.navigate().back();
+		Thread.sleep(2000);
+		logOut();
+		Thread.sleep(2000);
+		postCondition();
 
 	}
 
